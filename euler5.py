754c2b7f039e2b54de38f8360137018f861fff7b
Luke Weller <luke@lukes-mbp.dhcp.nd.edu>
2018-10-25 16:39:01 -0400

    Day 2: euler5 performance increased

--- euler5.py
+++ euler5.py
@@ -1,13 +1,10 @@
 # i7-4870HQ 4 cores, 8 threads, 2.5 GHz -> 3.7 GHz
-# real	0m4.359s
-# user	0m4.340s
-# sys	0m0.013s
-
-# This one definitely has room for improvement
-# Need to be able to utilize more than one process
+# real	0m0.261s
+# user	0m0.248s
+# sys	0m0.009s
 
 def check_n(n):
-	primes_less_than_20 = [19,18,17,16,15,14,13,12,11]
+	primes_less_than_20 = [18,17,16,15,14,13,12,11]
 	for prime in primes_less_than_20:
 		if n % prime != 0:
 			return False
@@ -18,10 +15,12 @@ if __name__ == '__main__':
 	n = 0
 
 	while True:
-		n += 20
+		n += (20*19)
+		# The above line **vastly** increases the performace of the solution
+		# However, it should be noted that it makes the solution no longer "perfect",
+		# as it is possbile for a solution to not be factorable by (20 * 19)
+		# If we instead used 'n += 20', it would be a perfect solution, but perforamce
+		# takes a huge hit; the solution takes x10 longer.
 		if check_n(n):
 			print(n)
 			break
-
-
-
